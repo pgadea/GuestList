7e8b909c465fee19ea04cefb2d6f20a6cc5da768
Pierre <pgadea7@gmail.com>
2017-07-29 11:40:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: '#app',
+    data: {
+        event: {
+            eventDate: 'August 14th - 16th',
+            eventTitle: 'Summer Festival!',
+            signUpText: 'Add your name to the guest list for <em>exclusive<em> offers:',
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        },
+        newNameText: '',
+        guestName: [],
+        appStyles: {
+            marginTop: '25px'
+        },
+        eventCapacity: 25,
+        eventCapacityPercentage: 0
+    },
+    methods: {
+        formSubmitted: function() {
+            if(this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
+                this.guestName.push(this.newNameText)
+                this.newNameText = ''
+                this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100)
+            }
+        }
+    }
+});
